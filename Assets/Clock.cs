751b1b29510f4d12db9962e9642a78f4018f0654
Alinda <Alindah@users.noreply.github.com>
2023-05-26 05:29:19 -0400

    Added clock

---
+++ Assets/Clock.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim.Play("");
+    }
+}
