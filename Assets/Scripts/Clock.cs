7a35b10a40b5c3705139d293a87b134e9f1d83d7
Alinda <Alindah@users.noreply.github.com>
2023-06-18 18:20:14 -0400

    Disable clocks on jump

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -4,16 +4,27 @@ public class Clock : MonoBehaviour
 {
     [SerializeField] private float ringDuration = 2.0f;
     [SerializeField] private float restDuration = 2.0f;
+    [SerializeField] private float gravityScale = 3.0f;
+
     private Animator anim;
+    private Rigidbody2D rb;
     private bool isRinging = false;
+    private bool playerIsTouching = false;
 
     private const string ANIM_REST = "clock_idle";
     private const string ANIM_RING = "clock_ring";
+    private const string PLAYER_TAG = "Player";
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         Invoke(nameof(Ring), 0.0f);
+    }
+
+    private void Update()
+    {
+        AnalyzePlayerContact();
     }
 
     private void Ring()
@@ -28,5 +39,26 @@ public class Clock : MonoBehaviour
         isRinging = false;
         anim.Play(ANIM_REST);
         Invoke(nameof(Ring), restDuration);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(PLAYER_TAG))
+            playerIsTouching = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(PLAYER_TAG))
+            playerIsTouching = false;
+
+    }
+
+    // Determine if player is turning off clock
+    private void AnalyzePlayerContact()
+    {
+        if (playerIsTouching && (Input.GetKeyDown(KeyCode.Space) || Input.GetAxis("Vertical") > 0))
+            rb.gravityScale = gravityScale;
     }
 }
